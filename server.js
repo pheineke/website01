2bb51a9a67a20c2779972659e4405953c7d97903
heineke.pb@edu.rptu.de <heineke.pb@edu.rptu.de>
2024-04-14 15:56:12 +0200

    ports

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 555555;
+const port = 55555;
 
 app.use(bodyParser.json());
 app.use(cors());
