c178b39aba7a1613b870bb3f632e146f3412124a
pheineke <heineke.pe@gmail.com>
2024-04-16 20:03:10 +0200

    Besucher eingabe fancy

--- js/input.js
+++ js/input.js
@@ -1,6 +1,21 @@
 function sendData() {
     var textInput = document.getElementById("textInput").value;
+    var user = document.getElementById("userInput").value;
     var outputDiv = document.getElementById("output");
+    var currentDate = new Date().toLocaleString(undefined, {
+        day:    'numeric',
+        month:  'numeric',
+        year:   'numeric',
+        hour:   '2-digit',
+        minute: '2-digit',
+    });
+    if (user == "") {
+        user = "Anonymous"
+    } else {
+        user = "JS ist manchmal etwas schwierig"
+    }
+    var data = [currentDate,user,textInput]
+    
 
     // AJAX request to send data to server
     var xhr = new XMLHttpRequest();
@@ -10,18 +25,13 @@ function sendData() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             // Server response
             var response = JSON.parse(xhr.responseText);
-            var currentDate = new Date().toLocaleString(undefined, {
-                day:    'numeric',
-                month:  'numeric',
-                year:   'numeric',
-                hour:   '2-digit',
-                minute: '2-digit',
-            });;
+            let content = response.text[2]
             
-            outputDiv.innerHTML += "<div class=\"container\">" + "<div class=\"comment_time\">" + currentDate + "</div" + response.text + "</div>";
+            outputDiv.innerHTML += "<div class=\"container\">" + "<div class=\"comment_time\">" + currentDate + "</div>" +"<div class=\"comment_user\">" + user + "</div>" + "<div class=\"comment_content\">" + content + "</div></div>";
         }
     };
-    xhr.send(JSON.stringify({ text: textInput }));
+    console.log(JSON.stringify({text : data}))
+    xhr.send(JSON.stringify({ text: data }));
 }
 
 window.onload = function() {
@@ -35,7 +45,10 @@ window.onload = function() {
             // Server response
             var response = JSON.parse(xhr.responseText);
             response.texts.forEach(function(text) {
-                outputDiv.innerHTML += "<div class=\"container\">" + text + "</div>";
+                let time = text[0];
+                let user = text[1];
+                let content = text[2];
+                outputDiv.innerHTML += "<div class=\"container\">" + "<div class=\"comment_time\">" + time + "</div>" + "<div class=\"comment_user\">" + user + "</div>" + " " + content + "</div>";
             });
         }
     };
