9befc990c46f90fd1a6e80d04b8fb127d74129f7
pheineke <heineke.pe@gmail.com>
2024-04-16 19:07:42 +0200

    besucher input

--- js/input.js
+++ js/input.js
@@ -4,13 +4,21 @@ function sendData() {
 
     // AJAX request to send data to server
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://linda.rhrk.uni-kl.de:55555/new", true); // Hier wird die IP-Adresse und der Port des Servers angegeben
+    xhr.open("POST", "http://localhost:3000/new", true); // Hier wird die IP-Adresse und der Port des Servers angegeben
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             // Server response
             var response = JSON.parse(xhr.responseText);
-            outputDiv.innerHTML += "<p>" + response.text + "</p>";
+            var currentDate = new Date().toLocaleString(undefined, {
+                day:    'numeric',
+                month:  'numeric',
+                year:   'numeric',
+                hour:   '2-digit',
+                minute: '2-digit',
+            });;
+            
+            outputDiv.innerHTML += "<div class=\"container\">" + "<div class=\"comment_time\">" + currentDate + "</div" + response.text + "</div>";
         }
     };
     xhr.send(JSON.stringify({ text: textInput }));
@@ -21,13 +29,13 @@ window.onload = function() {
 
     // AJAX request to fetch data from server
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://131.246.119.65:55555/history", true); // Hier wird die IP-Adresse und der Port des Servers angegeben
+    xhr.open("GET", "http://localhost:3000/history", true); // Hier wird die IP-Adresse und der Port des Servers angegeben
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             // Server response
             var response = JSON.parse(xhr.responseText);
             response.texts.forEach(function(text) {
-                outputDiv.innerHTML += "<p>" + text + "</p>";
+                outputDiv.innerHTML += "<div class=\"container\">" + text + "</div>";
             });
         }
     };
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const https = require('https');
 const fs = require('fs');
 
 const app = express();
-const port = 55555;
+const port = 3000;
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors()); // CORS aktivieren
 
 let texts = [];
 
@@ -16,19 +15,37 @@ app.post('/new', (req, res) => {
     const text = req.body.text;
     texts.push(text);
     res.json({ status: 'success', text: text });
+    
+    fs.writeFile("besucherlog.json", JSON.stringify(texts), 'utf8', (err) => {
+        if (err) {
+            console.error('Fehler beim Schreiben der Datei:', err);
+            return;
+        }
+        console.log('Array erfolgreich in die Datei gespeichert.');
+    });
 });
 
 app.get('/history', (req, res) => {
+    fs.readFile("besucherlog.json", 'utf8', (err, data) => {
+        if (err) {
+            // Bei einem Fehler, rufe die Callback-Funktion mit dem Fehler auf
+            console.log("Error beim Laden von besucherlog.json 0")
+            return;
+        }
+        try {
+            // Parst den Inhalt der Datei als JSON
+            texts = JSON.parse(data);
+            // Rufe die Callback-Funktion mit dem geparsten Array auf
+            return;
+        } catch (parseError) {
+            // Bei einem Fehler beim Parsen, rufe die Callback-Funktion mit dem Parsierungsfehler auf
+            return;
+        }
+    });
+
     res.json({ texts: texts });
 });
 
-// Lade das SSL-Zertifikat und den privaten Schlüssel
-const options = {
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-};
-
-// Erstelle den HTTPS-Server
-https.createServer(options, app).listen(port, () => {
-    console.log(`Server läuft auf https://linda.rhrk.uni-kl.de:${port}`);
+app.listen(port, () => {
+    console.log(`Server läuft auf http://localhost:${port}`);
 });
